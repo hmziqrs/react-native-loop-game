4c5c3b3281b7e3000f54a1b18db651ed49159e0e
hackerhgl <hackerhgl@gmail.com>
2020-03-14 15:33:58 +0500

    stable ios debug build

--- ios/rnloop/AppDelegate.m
+++ ios/rnloop/AppDelegate.m
@@ -10,6 +10,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <Firebase.h>
 
 @implementation AppDelegate
 
@@ -20,6 +21,9 @@
                                                    moduleName:@"rnloop"
                                             initialProperties:nil];
 
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
