5327edb3b0e0af521a515af8bf3374a483f6ba91
hmziqrs <hmziqrs@gmail.com>
2024-11-15 00:26:47 +0500

    Fixed screenshots for web

--- engine/capture.web.ts
+++ engine/capture.web.ts
@@ -5,12 +5,23 @@ export async function captureNShare(ref: React.RefObject<any>) {
   if (!node) {
     return;
   }
-  const dataUrl = await domtoimage.toPng(node);
+  const dataUrl = await domtoimage.toJpeg(node);
   const shareData = {
     title: "Loop Game",
     text: "Check out this level",
     url: dataUrl,
     dialogTitle: "Share this level",
   };
-  await navigator.share(shareData);
+  if (!navigator.canShare(shareData)) {
+    const link = document.createElement("a");
+    link.href = dataUrl;
+    link.download = `rn-loop-game-${new Date().getTime()}.jpg`;
+
+    // Append to document, trigger click, and remove
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  } else {
+    navigator.share(shareData);
+  }
 }
